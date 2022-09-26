b7b3021d6460a4a2bcda36ac072698d707f5fd03
Aditya Kumar <integrationoflogic@gmail.com>
2022-09-26 18:31:22 +0530

    Added DataBase part

--- ApachePOI/src/XLSXReaderExample.java
+++ ApachePOI/src/XLSXReaderExample.java
@@ -20,30 +20,26 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-
-
-
-
 public class XLSXReaderExample {
 	public static void main(String[] args) {
 		try {
-			//File file = new File("C:\\employee.xlsx"); // creating a new file instance
+			// File file = new File("C:\\employee.xlsx"); // creating a new file instance
 			File file = new File(".\\datafiles\\Countries.xlsx"); // creating
-			
-			FileInputStream files = new FileInputStream(file); 
+
+			FileInputStream files = new FileInputStream(file);
 			XSSFWorkbook workbook = new XSSFWorkbook(files);
-			XSSFSheet sheet = workbook.getSheetAt(0); 
-			Iterator<Row> itr = sheet.iterator(); 
+			XSSFSheet sheet = workbook.getSheetAt(0);
+			Iterator<Row> itr = sheet.iterator();
 			while (itr.hasNext()) {
 				Row row = itr.next();
-				Iterator<Cell> cellIterator = row.cellIterator(); 
+				Iterator<Cell> cellIterator = row.cellIterator();
 				while (cellIterator.hasNext()) {
 					Cell cell = cellIterator.next();
 					switch (cell.getCellType()) {
-					case STRING: 
+					case STRING:
 						System.out.print(cell.getStringCellValue() + "\t\t\t");
 						break;
-					case NUMERIC: 
+					case NUMERIC:
 						System.out.print(cell.getNumericCellValue() + "\t\t\t");
 						break;
 					default:
@@ -53,12 +49,12 @@ public class XLSXReaderExample {
 			}
 			workbook.close();
 		} catch (EncryptedDocumentException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-	    }  catch (IOException e) {
-	        // TODO Auto-generated catch block
-	        e.printStackTrace();
-	    }
-		
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
